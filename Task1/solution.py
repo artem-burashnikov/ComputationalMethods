c184d5de9034137887226a5ade6771725d935427
artem-burashnikov <artem.burashnikov@gmail.com>
2024-09-06 21:47:21 +0300

    feat: implement bisection method

--- Task1/solution.py
+++ Task1/solution.py
@@ -1,11 +1,30 @@
-def bisection():
+from collections.abc import Callable
+from Task1.constants import MyFloat
+
+
+def bisection(
+    f: Callable[[MyFloat], MyFloat], a: MyFloat, b: MyFloat, eps: MyFloat
+) -> tuple[MyFloat, MyFloat]:
+    while (b - a) > (2 * eps):
+        c = (a + b) / 2
+        if (f(a) * f(c)) <= 0:
+            b = c
+        else:
+            a = c
+
+    root = (a + b) / 2
+    delta = (b - a) / 2
+
+    return root, delta
+
+
+def newton() -> None:
     pass
 
-def newton():
+
+def modified_newton() -> None:
     pass
 
-def modified_newton():
-    pass
 
-def secant():
+def secant() -> None:
     pass
